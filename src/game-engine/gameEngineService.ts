53a742fbfc1d9d94c1b8ba417efb3c1154a273e6
David Kim <david.kim@demandmedia.com>
2021-03-24 15:05:00 -0700

    Adds create deck

--- src/game-engine/gameEngineService.ts
+++ src/game-engine/gameEngineService.ts
@@ -1,3 +1,16 @@
+
+const createDeck = () => {
+    const deck = []
+    const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    const suits = ['Spades','Hearts','Diamonds','Clubs']
+    for (let i = 0; i < suits.length; i++) {
+        for (let x = 0; x < cardValue.length; x++) {
+						deck.push([cardValue[x], suits[i]])
+        }
+    }
+    return deck;
+}
+console.log(createDeck())
 // gameStart()
 // shuffle()
 // deal()
